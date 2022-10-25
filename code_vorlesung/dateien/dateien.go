e703b8ce74ca0dce84a01dc73fd6ba0264a87180
Reiner Hüchting <dev@reinerhuechting.de>
2022-10-25 13:37:10 +0200

    Kommentare ergänzt.

--- code_vorlesung/dateien/dateien.go
+++ code_vorlesung/dateien/dateien.go
@@ -10,12 +10,19 @@ import (
 
 func main() {
 
+	// Eine Datei einlesen.
 	lines := ReadLinesFromFile("wetter.csv")
 
+	// Die Daten auswerten.
 	fmt.Println("Temperaturen am Morgen:")
+
+	// Für jede gelesene Zeile...
 	for _, line := range lines {
+
+		// ... die Zeile auftrennen in eine Liste von einzelnen Strings.
 		data := strings.Split(line, ";")
-		//fmt.Println(data)
+
+		// ... ausgewählte Daten aus der Liste ausgeben.
 		fmt.Printf("    %v: %v Grad\n", data[0], data[1])
 	}
 	fmt.Println("Temperaturen am Mittag:")
@@ -28,13 +35,23 @@ func main() {
 
 // Liest Zeilen aus einer CSV-Datei und liefert sie als ...
 func ReadLinesFromFile(fileName string) []string {
+
+	// Eine Datei öffnen.
+	// os.Open() erwartet einen Dateinamen und liefert ein File- und ein Error-Objekt.
 	file, err := os.Open(fileName)
+
+	// Err ist nicht nil (d.h. es existiert), falls die Datei nicht geöffnet werden konnte.
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Die Datei schließen.
+	defer file.Close()
+
+	// Scanner erzeugen. Ein Scanner kann Datenströme lesen und interpretieren.
 	scanner := bufio.NewScanner(file)
 
+	// Mit dem Scanner Zeile für Zeile lesen und als Strings liefern.
 	result := []string{}
 	for scanner.Scan() {
 		result = append(result, scanner.Text())
